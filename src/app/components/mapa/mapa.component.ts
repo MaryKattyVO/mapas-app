49d89a1f2960f79baea28955b59341ef5f0a8a5b
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-26 15:11:37 -0500

    SnackBar: Notificación cuando se borra un punto en el mapa

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Marcador } from '../../classes/marcador.class';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-mapa',
@@ -11,7 +12,7 @@ export class MapaComponent implements OnInit {
   marcadores: Marcador[] = [];
   lat: number = 51.678418;
   lng: number = 7.809007;
-  constructor() {
+  constructor(private snackBar: MatSnackBar ) {
     if(localStorage.getItem('marcadores')) {
       this.marcadores = JSON.parse(localStorage.getItem('marcadores')!);
     }
@@ -26,13 +27,16 @@ export class MapaComponent implements OnInit {
     const nuevoMarcador = new Marcador(coords.lat, coords.lng)
     this.marcadores.push(nuevoMarcador);
     this.guardarStorage();
+    this.snackBar.open('Marcador agregado', 'Cerrar', {duration: 3000});
   }
 
   borrarMarcador(i: any){
     this.marcadores.splice(i, 1);
     this.guardarStorage();
+    this.snackBar.open('Marcador borrado', 'Cerrar', {duration: 3000});
   }
   guardarStorage() {
     localStorage.setItem('marcadores', JSON.stringify(this.marcadores))
+    
   }
 }
